848937eea351197560385dc0e08af8ea9a82de87
Kharisma Wahyudin <kharismatics@gmail.com>
2020-09-02 09:54:35 +0700

    basic vue router

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,25 @@
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import VueRouter from 'vue-router';
+
+window.Vue.use(VueRouter);
+
+import ExampleComponent from './components/ExampleComponent.vue';
+import TesComponent from './components/TesComponent.vue';
+
+const routes = [
+    {
+        path: '/',
+        component: ExampleComponent
+    },
+    {
+        path: '/tes',
+        component: TesComponent
+    }
+]
+
+const router = new VueRouter({ routes })
+
+const app = new Vue({ router }).$mount('#app')
