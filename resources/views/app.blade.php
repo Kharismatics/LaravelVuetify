2c26d008be963a09f5b4604ebc0edb3915241a15
Kharisma Wahyudin <kharismatics@gmail.com>
2020-11-10 10:47:03 +0700

    add laravel pwa

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -11,14 +11,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    {{-- service workers --}}
-    <script>
-        if ('serviceWorker' in navigator) {
-            window.addEventListener('load', () => {
-                navigator.serviceWorker.register('/sw.js');
-            });
-        }
-    </script>
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
@@ -37,8 +29,7 @@
     <link href="https://cdn.jsdelivr.net/npm/vuetify@2.x/dist/vuetify.min.css" rel="stylesheet">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
 
-    {{-- manifest --}}
-    <link rel="manifest" href="manifest.json" />
+    @laravelPWA
 </head>
 <body>
     <div id="app">
---
+++ resources/views/vendor/laravelpwa/offline.blade.php
@@ -0,0 +1,19 @@
+{{-- @extends('layouts.app')
+
+@section('content')
+
+    <h1>You are currently not connected to any networks.</h1>
+
+@endsection --}}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>offline</div>
+</body>
+</html>
